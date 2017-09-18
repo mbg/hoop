0895385427bb665d543ea35bc88cc7428ee25b37
Michael Gale <m.gale@warwick.ac.uk>
2017-09-18 12:12:50 +0100

    Clean up Selectors.hs and implement more composition functions

--- Language/MSH/Selectors.hs
+++ Language/MSH/Selectors.hs
@@ -7,48 +7,42 @@
 
 module Language.MSH.Selectors where
 
+--------------------------------------------------------------------------------
+
 import Control.Applicative ((<$>))
 import Control.Monad.Identity
 import Control.Monad.State
 
--- | Enumerates call types.
---data CallType = ExtCall     -- ^ The call is to a method, externally.
+--------------------------------------------------------------------------------
 
--- | Represents a query which can be run by combinators such as `result', `object', etc.
-data RunnableQuery {-(ty :: CallType)-} obj st ctx r where
-    MkExtCall  :: ctx (r, obj) -> RunnableQuery {-ExtCall-} obj st ctx r
+-- | Represents a query which can be run by combinators such as `result',
+-- `object', etc.
+data RunnableQuery obj st ctx r where
+    MkExtCall  :: ctx (r, obj) -> RunnableQuery obj st ctx r
 
--- | Represents a context in which combinators may be used.
-class Functor ctx => CallCtx {-(ty :: CallType)-} ctx where
-    --type CtxResult ty ctx r :: *
+-- | `result` @selector obtains the result from invoking @selector.
+result :: (ctx ~ Identity) => RunnableQuery obj st ctx r -> r
+result (MkExtCall call) = fst $ runIdentity call
 
-    result :: RunnableQuery {-ty-} obj st ctx r -> r -- CtxResult ty ctx r
-    object :: RunnableQuery {-ty-} obj st ctx r -> obj -- CtxResult ty ctx obj
-    --(<:) :: RunnableQuery ty obj ctx r -> r -> ctx ()
+-- | `object` @selector obtains an updated object from invoking @selector.
+object :: (ctx ~ Identity) => RunnableQuery obj st ctx r -> obj
+object (MkExtCall call) = snd $ runIdentity call
 
-{-instance Functor ctx => CallCtx ThisCall ctx where
-    type CtxResult ThisCall ctx r = ()
+-- | Enumerates types of object class members.
+data MemberType = Method | Field
 
-    getResult _ = ()
-    getObject _ = ()-}
+-- | Determines the type of selector that results from composing two
+-- selectors.
+type family MemberComposeResult (lhs :: MemberType) (rhs :: MemberType) :: MemberType where
+    MemberComposeResult Method Method = Method
+    MemberComposeResult Method Field  = Method
+    MemberComposeResult Field  Method = Method
+    MemberComposeResult Field  Field  = Field
 
-instance (ctx ~ Identity) => CallCtx {-ExtCall-} ctx where
-    --type CtxResult ExtCall ctx r = r
+--type Selector ty o s m a =
 
-    result (MkExtCall call) = fst $ runIdentity call
-    object (MkExtCall call) = snd $ runIdentity call
-    --(<:) (MkExtCall call) v = undefined
-
-data MemberType = Mutable | Immutable
-data FieldType = Method | Field
-
-type family FieldComposeResult (lhs :: FieldType) (rhs :: FieldType) :: FieldType where
-    FieldComposeResult Method Method = Method
-    FieldComposeResult Method Field  = Method
-    FieldComposeResult Field  Method = Method
-    FieldComposeResult Field  Field  = Field
-
-data Selector (ty :: FieldType) o s m a where
+-- | A selector is a container for object class members.
+data Selector (ty :: MemberType) o s m a where
     MkMethod :: StateT s m a ->
                 (o -> m (a, o)) ->
                 Selector Method o s m a
@@ -58,12 +52,13 @@ data Selector (ty :: FieldType) o s m a where
                 (a -> StateT s m ()) ->
                 Selector Field o s m a
 
+-- | Proxy type used to capture the type variables of an object.
 data This o s (m :: * -> *) a where
     MkThis :: This o s m a
 
 type family QueryObject obj :: *
 type family QueryMonad obj (m :: * -> *) :: * -> *
-type family QueryResult obj (ty :: FieldType) st (m :: * -> *) r :: *
+type family QueryResult obj (ty :: MemberType) st (m :: * -> *) r :: *
 
 
 infixr 8 .!
@@ -78,21 +73,54 @@ class Monad m => Object obj st m where
 
 -- | If `s' returns a value whose type is a `Functor', then `s.$m' calls `m' on the
 --   inner value of `s' via `fmap'.
-(.$) :: (Monad ctx, Functor f) => Selector lty obj st ctx (f a) -> Selector rty a st' Identity b -> Selector (FieldComposeResult lty rty) obj st ctx (f b)
+(.$) :: (Monad ctx, Functor f) =>
+        Selector lty obj st ctx (f a) ->
+        Selector rty a st' Identity b ->
+        Selector (MemberComposeResult lty rty) obj st ctx (f b)
 (MkField eg ig es is) .$ (MkMethod ri re) = MkMethod
-    (ig >>= \x -> let p = fmap (runIdentity . re) x in is (fmap snd p) >> return (fmap fst p))
-    (\s -> eg s >>= \(x,s') -> let p = fmap (runIdentity . re) x in es s' (fmap snd p) >>= \(_,s'') -> return (fmap fst p, s''))
+    (do box <- ig
+        let r = fmap (runIdentity . re) box
+        is (fmap snd r)
+        return (fmap fst r))
+    (\this -> do (box,this') <- eg this
+                 let r = fmap (runIdentity . re) box
+                 (_,this'') <- es this' (fmap snd r)
+                 return (fmap fst r, this''))
 (MkField eg ig es is) .$ (MkField reg rig res ris) = MkField
+    (\this -> do (box,this') <- eg this
+                 let r = fmap (runIdentity . reg) box
+                 (_,this'') <- es this' (fmap snd r)
+                 return (fmap fst r, this''))
+    (do box <- ig
+        let r = fmap (runIdentity . reg) box
+        is (fmap snd r)
+        return (fmap fst r))
+    -- TODO: the definition of Selector is too restrictive to
+    -- implement the following two cases: we need to split the 'a' type
+    -- parameter into two: one for the type of values to get and one for
+    -- the type of values to set
     undefined
+    {-(\this v -> do (box,this') <- eg this
+                   let r = fmap (\obj -> runIdentity (res obj v)) box
+                   (_,this'') <- es this' (fmap snd r)
+                   return ((), this''))-}
     undefined
-    undefined
-    undefined
-(MkMethod li le) .$ (MkMethod ri re) = MkMethod
-    undefined
-    undefined
-(MkMethod li le) .$ (MkField reg rig res ris) = MkMethod
-    undefined
-    undefined
+    {-(\v -> do box <- ig
+              )-}
+(MkMethod li le) .$ (MkMethod _ re) = MkMethod
+    (do box <- li
+        let r = fmap (runIdentity . re) box
+        return (fmap fst r))
+    (\this -> do (box, this') <- le this
+                 let r = fmap (runIdentity . re) box
+                 return (fmap fst r, this'))
+(MkMethod li le) .$ (MkField reg _ _ _) = MkMethod
+    (do box <- li
+        let r = fmap (runIdentity . reg) box
+        return (fmap fst r))
+    (\this -> do (box, this') <- le this
+                 let r = fmap (runIdentity . reg) box
+                 return (fmap fst r, this'))
 
 {-
 type instance QueryMonad (RunnableQuery ThisCall obj st ctx r) ctx' = ctx
@@ -153,12 +181,20 @@ instance (Object obj st ctx, Object r st' Identity, m ~ Identity) =>
     Object (Selector Method obj st ctx r) st' m where
 
     (.!) (MkMethod li le) (MkMethod ri re) = MkMethod
-        (li >>= \r -> return $ fst $ runIdentity $ re r)
-        (\s -> le s >>= \(r, obj) -> return (fst $ runIdentity $ re r, obj))
+        (do obj <- li
+            let (r,obj') = runIdentity (re obj)
+            return r)
+        (\this -> do (obj,this') <- le this
+                     let (r,obj') = runIdentity (re obj)
+                     return (r, this'))
 
     (.!) (MkMethod li le) (MkField eg ig es is) = MkMethod
-        (li >>= \r -> return $ fst $ runIdentity $ eg r)
-        (\s -> le s >>= \(r, obj) -> return (fst $ runIdentity $ eg r, obj))
+        (do obj <- li
+            let (r,obj') = runIdentity (eg obj)
+            return r)
+        (\this -> do (obj, this') <- le this
+                     let (r,obj') = runIdentity (eg obj)
+                     return (r, this'))
 
 -- For fields:
 -- * run the internal call (if the selector on the RHS is a method)
@@ -175,14 +211,38 @@ instance (Object obj st ctx, Object r st' Identity, m ~ Identity) =>
     -- get the value of the field on the LHS, run the RHS method on it, then
     -- set the value of the field to the object returned by the RHS method
     (.!) (MkField eg ig es is) (MkMethod ri re) = MkMethod
-        (ig >>= \r -> let (r',s) = runIdentity (re r) in is s >> return r')
-        (\s -> eg s >>= \(r',s') -> let (r'',s'') = runIdentity (re r') in es s' s'' >>= \(_,s''') -> return (r'',s'''))
+        (do obj <- ig
+            -- * obj is the value (an object) of the field on the left
+            -- * use obj as object for the method on the right
+            let (r,obj') = runIdentity (re obj)
+            -- * update the current object with obj', the updated obj
+            is obj'
+            -- * return the result of the method on the right
+            return r)
+        (\this -> do (obj,this') <- eg this
+                     -- * use obj as object for the method on the right
+                     let (r,obj') = runIdentity (re obj)
+                     -- * update the current object with s, the updated r
+                     (_,this'') <- es this' obj'
+                     return (r,this''))
 
     (.!) (MkField eg ig es is) (MkField reg rig res ris) = MkField
-        undefined
-        undefined
-        undefined
-        undefined
+        (\this -> do (obj,this') <- eg this
+                     let (r,obj') = runIdentity (reg obj)
+                     (_,this'') <- es this' obj'
+                     return (r,this''))
+        (do obj <- ig
+            let (r,obj') = runIdentity (reg obj)
+            is obj'
+            return r)
+        (\this v -> do (obj,this') <- eg this
+                       let (r,obj') = runIdentity (res obj v)
+                       (_,this'') <- es this' obj'
+                       return (r,this''))
+        (\v -> do obj <- ig
+                  let (r,obj') = runIdentity (res obj v)
+                  is obj'
+                  return r)
 
 -- For `this':
 -- * run the internal call (if the selector on the RHS is a method)
